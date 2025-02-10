1a923a6f7a20f90724f646403c2576816e745235
shahadathhs <shahadathhossensajib732@gmail.com>
2025-02-10 20:35:51 +0600

    chore: updated tailwind css to v4 and removed shadcn

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,16 +1,9 @@
 // * Home page ("/")
-import { TextAnimate } from "@/components/magicui/text-animate";
-import { Button } from "@/components/ui/button";
-import { TextRevealCardPreview } from "@/lib/components/TextRevealCardPreview";
 
 export default function Home() {
   return (
     <div>
-      <Button>Click me</Button>
-      <TextAnimate animation="slideUp" by="word">
-        Slide up by word
-      </TextAnimate>
-      <TextRevealCardPreview />
+      This is the home page
     </div>
   );
 }
