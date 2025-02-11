9191df794a76dd43aceac1b6f8d9ed04051c65c3
shahadathhs <shahadathhossensajib732@gmail.com>
2025-02-11 15:20:08 +0600

    chore: daisy ui added

--- lib/shared/Navbar.tsx
+++ lib/shared/Navbar.tsx
@@ -1,5 +1,36 @@
+import Link from "next/link";
+const navItems = [
+  { label: "Home", route: "/" },
+  { label: "Blogs", route: "/blogs" },
+  { label: "Contact", route: "/contact" },
+  { label: "Projects", route: "/projects" },
+];
+
 export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link href="/admin">Dashboard</Link>
+          </li>
+          <li>
+            <details>
+              <summary>Menu</summary>
+              <ul className="bg-base-100 rounded-t-none p-2">
+                {navItems.map((item) => (
+                  <li key={item.label}>
+                    <Link href={item.route}>{item.label}</Link>
+                  </li>
+                ))}
+              </ul>
+            </details>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
