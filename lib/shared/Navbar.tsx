87f308fa7fb40e7130c54492fc6c2f1a607faf10
shahadathhs <shahadathhossensajib732@gmail.com>
2025-02-12 21:12:06 +0600

    feat: fixed logo & favicon

--- lib/shared/Navbar.tsx
+++ lib/shared/Navbar.tsx
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import Link from "next/link";
+
 const navItems = [
   { label: "Home", route: "/" },
   { label: "Blogs", route: "/blogs" },
@@ -10,7 +12,15 @@ export default function Navbar() {
   return (
     <div className="navbar bg-base-100 rounded-sm shadow-md container mx-auto px-4">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <Link href="/">
+          <Image
+            src="/logo.png"
+            alt="Logo"
+            className="rounded-sm"
+            width={100}
+            height={40}
+          />
+        </Link>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
